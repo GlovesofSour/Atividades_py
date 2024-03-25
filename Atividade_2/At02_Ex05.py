aa61c4b4710d33b47861da52fba9a59fbe7db410
Lucas Paulino <112363220+GlovesofSour@users.noreply.github.com>
2024-03-25 10:05:56 -0300

    Atividade 2

---
+++ Atividade_2/At02_Ex05.py
@@ -0,0 +1,8 @@
+num = int(input('Digite um valor: '))
+
+resultado=0
+
+for i in range(0,num):
+    resultado += 1+i*2
+    
+print(f'O quadrado ne {num} é {resultado}')
--- Atividade_2/At02_Ex07.py
+++ Atividade_2/At02_Ex07.py
@@ -1,13 +1,11 @@
-num = input("Digite o número da conta (até 6 dígitos): ")
+num = int(input("Digite o número da conta (até 6 dígitos): "))
 soma = 0
 
-for i in num:
-    soma += int(num)
+a = int(num/100000)
+b = int((num%100000)/10000)
+c = int((num%10000)/1000)
+d = int((num%1000)/100)
+e = int(num%100/10)
+f = int(num%10)
 
-d = soma % 10
-
-tam =  6 - len(num)
-
-conta = "0" * tam + num + "-" + str(d)
-
-print("Número completo da conta:", conta)
+print(f"Número completo da conta: {num:0>6d}-{(a+b+c+d+e+f)%10}")
